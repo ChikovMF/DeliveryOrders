20d5ed508f6b53d7372fa3248b0ed34e89fe93bd
Matvey Chikov <ChikovMF@yandex.ru>
2026-08-19 12:52:02 +0300

    Add initial project structure and Order class definition

---
+++ Domain/Order.cs
@@ -0,0 +1,42 @@
+﻿namespace Domain;
+
+/// <summary>
+/// Заказ.
+/// </summary>
+public sealed class Order
+{
+    /// <summary>
+    /// Идентификатор.
+    /// </summary>
+    public int Id { get; set; }
+
+    /// <summary>
+    /// Город отправителя.
+    /// </summary>
+    public string SenderCity { get; set; }
+
+    /// <summary>
+    /// Адрес отправителя.
+    /// </summary>
+    public string SenderAddress { get; set; }
+
+    /// <summary>
+    /// Город получателя.
+    /// </summary>
+    public string RecipientCity { get; set; }
+
+    /// <summary>
+    /// Адрес получателя.
+    /// </summary>
+    public string RecipientAddress { get; set; }
+
+    /// <summary>
+    /// Вес.
+    /// </summary>
+    public decimal Weight { get; set; }
+
+    /// <summary>
+    /// Дата и время забора.
+    /// </summary>
+    public DateTimeOffset PickupDate { get; set; }
+}
