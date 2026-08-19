c08aa3dffaedd0f6eec6c939c5758af7b4cd6108
Matvey Chikov <ChikovMF@yandex.ru>
2026-08-19 15:57:01 +0300

    Add ServiceCollectionExtensions for database context configuration and order repository registration

---
+++ Store/ServiceCollectionExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Domain.Orders;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Store.Repositories;
+
+namespace Store;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddDbContext(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<IAppDbContext, AppDbContext>(options =>
+        {
+            options.UseNpgsql(
+                configuration.GetConnectionString("PostgreSQL"));
+        });
+
+        services.TryAddScoped<IOrderRepository, OrderRepository>();
+
+        return services;
+    }
+}
